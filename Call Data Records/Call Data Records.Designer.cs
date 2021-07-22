812e4db4785c804b65e1c5ca68dc1d4fbba41d5a
DaNiElE-1999 <87183305+DaNiElE-1999@users.noreply.github.com>
2021-07-22 19:51:19 +0200

    Add files via upload

---
+++ Call Data Records/Call Data Records.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Assignment_2
+{
+    partial class CallDataRecords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TimeOfCall = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Destination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AreaCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Connection = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PPm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Insert = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TimeOfCall,
+            this.Duration,
+            this.Destination,
+            this.AreaCode,
+            this.Connection,
+            this.PPm,
+            this.Total});
+            this.dataGridView1.Location = new System.Drawing.Point(58, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(788, 235);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // TimeOfCall
+            // 
+            this.TimeOfCall.HeaderText = "Time Of Call";
+            this.TimeOfCall.Name = "TimeOfCall";
+            this.TimeOfCall.Width = 200;
+            // 
+            // Duration
+            // 
+            this.Duration.HeaderText = "Duration";
+            this.Duration.Name = "Duration";
+            // 
+            // Destination
+            // 
+            this.Destination.HeaderText = "Destination";
+            this.Destination.Name = "Destination";
+            // 
+            // AreaCode
+            // 
+            this.AreaCode.HeaderText = "Area Code";
+            this.AreaCode.Name = "AreaCode";
+            // 
+            // Connection
+            // 
+            this.Connection.HeaderText = "Connection";
+            this.Connection.Name = "Connection";
+            // 
+            // PPm
+            // 
+            this.PPm.HeaderText = "PPM";
+            this.PPm.Name = "PPm";
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(333, 288);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(238, 29);
+            this.Insert.TabIndex = 1;
+            this.Insert.Text = "Upload";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(427, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(310, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Insert Destination";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(743, 262);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(93, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Insert Duration";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "yyyy-mm-dd HH:mm:ss";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(51, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Insert Date ->";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CallDataRecords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Assignment_2.Properties.Resources.mechanical_clock_3d_1;
+            this.ClientSize = new System.Drawing.Size(906, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CallDataRecords";
+            this.Text = "Call Data Records";
+            this.Load += new System.EventHandler(this.CallDataRecords_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeOfCall;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Destination;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AreaCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Connection;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PPm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Call Data Records/Call Data Records.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2
+{
+    public partial class CallDataRecords : Form
+    {
+        public CallDataRecords()
+        {
+            InitializeComponent();
+        }
+
+        private void CallDataRecords_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            DataTable CDR = new DataTable();
+            CDR.Columns.Add("Time of Call");
+            CDR.Columns.Add("Duration");
+            CDR.Columns.Add("Destination");
+            CDR.Columns.Add("Area Code");
+            CDR.Columns.Add("Connection");
+            CDR.Columns.Add("PPM");
+            CDR.Columns.Add("Total");
+
+            DataRow row = CDR.NewRow();
+
+            
+            
+            int duration = int.Parse(textBox3.Text);
+
+            
+
+            string prefix0 = textBox2.Text;
+            string prefix = prefix0.Substring(0, 2);
+            double connection, ppm, total;
+            String areaCode;
+            switch (prefix)
+            {
+                case "02":
+                case "03":
+                    areaCode = "Domestic";
+                    connection = 0;
+                    ppm = 0.5;
+                    break;
+                case "07":
+                    areaCode = "Mobile";
+                    connection = 0;
+                    ppm = 5.5;
+                    break;
+                case "00":
+                    areaCode = "International";
+                    connection = 5;
+                    ppm = 11.3;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Wrong input");
+            }//switch
+
+            total = duration * ppm + connection;
+
+            row["Time of Call"] = textBox1.Text;
+            row["Duration"] = textBox3.Text;
+            row["Destination"] = textBox2.Text;
+            row["Area Code"] =areaCode;
+            row["Connection"] = connection;
+            row["PPM"] = ppm;
+            row["Total"] = total;
+            CDR.Rows.Add(row);
+
+            addRecord(textBox1.Text, textBox3.Text, textBox2.Text, areaCode, connection, ppm, total);
+
+            foreach (DataRow Drow in CDR.Rows)
+            {
+                int num = dataGridView1.Rows.Add();
+                dataGridView1.Rows[num].Cells[0].Value = Drow["Time of Call"].ToString();
+                dataGridView1.Rows[num].Cells[1].Value = Drow["Duration"].ToString();
+                dataGridView1.Rows[num].Cells[2].Value = Drow["Destination"].ToString();
+                dataGridView1.Rows[num].Cells[3].Value = Drow["Area Code"].ToString();
+                dataGridView1.Rows[num].Cells[4].Value = Drow["Connection"].ToString();
+                dataGridView1.Rows[num].Cells[5].Value = Drow["PPM"].ToString();
+                dataGridView1.Rows[num].Cells[6].Value = Drow["Total"].ToString();
+
+            }//foreach
+
+        }//insertclick
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void addRecord(string time, string duration, string destination, string area, double connection, double ppm, double total)
+        {
+           
+
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Daniele\source\repos\Assignment 2\Assignment 2\CSV.txt", true))
+                {
+                    file.WriteLine(time + ", "+ duration + ", " + destination + ", " + area + ", " + connection + ", " + ppm + ", " + total);
+                }
+            }//try
+            catch(Exception ex)
+            {
+                throw new ApplicationException("This application did an oopsie",ex);
+
+            }//catch
+        }
+    }
+}
